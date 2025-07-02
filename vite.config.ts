c6c84bf7c9f407b8914f1a83b74fe79887fa3bdf
LAPTOP-V5IC4G6U\Adil Talukdar <adil.talukdar12j@gmail.com>
2025-07-02 10:12:18 +0530

    Fix: Clean reinstall to resolve Vercel permissions

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  eslint:{
+      ignoreDuringBuild: true,
+      fix: true,
+  },
   plugins: [
     react(),
     mode === 'development' &&
